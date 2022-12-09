1e78ad82d6a372a8b9f70bb2cb70cf23c7c2d1ab
Mugich900 <ilus200129@gmail.com>
2022-12-09 14:47:00 +0300

    add new html files

--- ElBook/urls.py
+++ ElBook/urls.py
@@ -19,4 +19,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('library.urls')),
+    path(''+'authorization.html', include('library.urls')),
+    path(''+'registration.html', include('library.urls')),
 ]
--- library/views.py
+++ library/views.py
@@ -2,5 +2,11 @@ from django.shortcuts import render
 
 # Create your views here.
 
-def post_list(request):
-    return render(request, 'library/post_list.html', {})
+def index(request):
+    return render(request, 'library/index.html', {})
+
+def authorization(request):
+    return render(request, 'library/authorization.html', {})
+
+def registration(request):
+    return render(request, 'library/registration.html', {})
