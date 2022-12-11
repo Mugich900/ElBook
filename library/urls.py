afe0c746719b64fc2134b2902a390b7d3b862542
Mugich900 <ilus200129@gmail.com>
2022-12-12 02:09:41 +0300

    Editing publication

--- library/urls.py
+++ library/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('authorization/', views.authorization, name='authorization'),
     path('registration/', views.registration, name='registration'),
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
+    path('post/new/', views.post_new, name='post_new'),
+    path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
 ]
